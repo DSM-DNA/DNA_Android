eb115e0caa402550946bcb8f8e7dedb004848e9f
Ko-GyeongTae <kokt0203@naver.com>
2021-02-11 23:12:57 +0900

    Add: Svg

--- components/NavController.js
+++ components/NavController.js
@@ -4,7 +4,7 @@ import AuthNavigation from "../navigation/AuthNavigation";
 import MainNavigation from "../navigation/MainNavigation";
 
 export default () => {
-    const isLoggedIn = true;
+    const isLoggedIn = false;
     return (
         <View style={{flex:1}}>
             {isLoggedIn ? <MainNavigation/> : <AuthNavigation/>}
--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Image, Text } from "react-native";
+import { Text } from "react-native";
 import styled from "styled-components";
+import GlobeSvg from '../../assets/images/globe.js';
 
-
-const View = styled.View``;
+const View = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    background-color: #204051;
+`;
 
 
 export default ({ navigation }) => (
     <View>
         <Text>Auth Home</Text>
-
+        <GlobeSvg />
     </View>
 );
