fa9c71aef05262bc7a0c06bea6bd06b779e56fb7
Ko-GyeongTae <kokt0203@naver.com>
2021-02-13 02:43:04 +0900

    Edit: AuthBackground

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import AuthBackground from "../../assets/images/AuthBackground";
 
 const View = styled.View`
-  background-color: white;
+  background-color: #204051;
+  justify-content: flex-end;
+  align-items: center;
+  flex: 45;
+`;
+
+const _View = styled.View`
+  background-color: #ffffff;
   justify-content: center;
   align-items: center;
-  flex: 1;
+  flex: 55;
 `;
 
 const Text = styled.Text`
@@ -13,7 +21,12 @@ const Text = styled.Text`
 `;
 
 export default () => (
-  <View>
-    <Text>Login</Text>
-  </View>
+  <>
+    <View>
+      <AuthBackground/>
+    </View>
+    <_View>
+      <Text>Hello</Text>
+    </_View>
+  </>
 );
