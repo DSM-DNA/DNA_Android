5f71fa9e50c67cdf93fcf17e71567df35f8ca07c
Ko-GyeongTae <kokt0203@naver.com>
2021-02-16 00:25:40 +0900

    SelectBox studying

--- screens/Auth/Logout.js
+++ screens/Auth/Logout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Selectbox from "react-native-selectbox";
+import { useState } from "react/cjs/react.development";
 
 const View = styled.View`
   background-color: white;
@@ -12,8 +14,21 @@ const Text = styled.Text`
   color: black;
 `;
 
-export default () => (
-  <View>
-    <Text>Logout</Text>
-  </View>
-);
+export default () => {
+  const items = [
+    { key: 0, label: 'Fruits', value:'some value' },
+    { key: 1, label: 'Fruit', value:'hello' },
+  ];
+  const onChange = (item) => {
+    console.log(item);
+  }
+  return (
+    <View>
+      <Text>Logout</Text>
+      <Selectbox
+        selectedItem={items[0]}
+        onChange={onChange}
+        items={items} />
+    </View>
+  );
+};
