9af2c71ac0b09d4c17dd64c8cca8f32237568c3f
Ko-GyeongTae <kokt0203@naver.com>
2021-02-17 21:14:51 +0900

    Edit: Logout

--- screens/Tabs/Timelines.js
+++ screens/Tabs/Timelines.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import styled from "styled-components";
 import Adenine from "../../assets/images/Adenine";
 import Category from "../../assets/images/Category";
@@ -7,6 +9,12 @@ import Guanine from "../../assets/images/Guanine";
 import MainHeader from "../../assets/images/MainHeader";
 import MainNotice from "../../assets/images/MainNotice";
 import Thymine from "../../assets/images/Thymine";
+import { useLogOut } from "../../AuthContext";
+import Logout from "../Auth/Logout";
+import { useState } from "react/cjs/react.development";
+import { Alert } from "react-native";
+
+const baseUri = "http://121.66.14.43:9191";
 
 const View = styled.View`
   background-color: white;
@@ -16,6 +24,7 @@ const View = styled.View`
 
 const Header = styled.View`
   flex-direction: row;
+  align-items: center;
   margin-top: 54px;
   width: 80%;
   height: 18%;
@@ -62,10 +71,10 @@ const green = "#CAE8D5";
 const white = "#ffffff";
 
 const Container = styled.TouchableOpacity`
-  background-color: ${props => props.bgColor};
+  background-color: ${(props) => props.bgColor};
   width: 190px;
   height: 118px;
-  borderRadius: 16px;
+  border-radius: 16px;
   padding-bottom: 30px;
   padding-left: 7px;
   elevation: 3;
@@ -73,49 +82,96 @@ const Container = styled.TouchableOpacity`
 //width: 206px height 190px;
 const Text = styled.Text`
   color: black;
+  font-size: 15px;
 `;
 
-export default ({navigation}) => (
-  <>
-  <View>
-    <Header>
-      <MainHeader />
-      <Text>Hello</Text>
-    </Header>
-    <Notice>
-      <MainNotice />
-    </Notice>
-    <MidCategory>
-      <Category />
-    </MidCategory>
-  </View>
-  <Grid>
-    <EachButton>
-      <Container bgColor={white} onPress={() => navigation.navigate("A_tab")}>
-        <Adenine />
-      </Container>
-    </EachButton>
-    <EachButton>
-      <Container bgColor={green} onPress={() => navigation.navigate("T_tab")}>
-        <Thymine />
-      </Container>
-    </EachButton>
-  </Grid>
-  <Grid>
-    <EachButton>
-      <Container bgColor={green} onPress={() => navigation.navigate("G_tab")}>
-        <Guanine />
-      </Container>
-    </EachButton>
-    <EachButton>
-      <Container bgColor={white} onPress={() => navigation.navigate("C_tab")}>
-        <Cytosine />
-      </Container>
-    </EachButton>
-  </Grid>
-  <Bottom>
+export default ({ navigation }) => {
+  const logOut = useLogOut();
 
-  </Bottom>
-  </>
+  const Logout = async () => {
+    const token = "";
+    axios
+      .get(`${baseUri}/logout?${token}`)
+      .then(function (response) {
+        console.log(response);
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    logOut();
+  };
 
-);
+  const confirmAlert = () => {
+    Alert.alert(
+      "로그아웃 하시겠습니까?",
+      "",
+      [
+        {
+          text: "No",
+          onPress: () => null,
+        },
+        {
+          text: "Yes",
+          onPress: () => Logout(),
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+  return (
+    <>
+      <View>
+        <Header>
+          <MainHeader />
+          <TouchableOpacity onPress={() => confirmAlert()}>
+            <Text>Logout</Text>
+          </TouchableOpacity>
+        </Header>
+        <Notice>
+          <MainNotice />
+        </Notice>
+        <MidCategory>
+          <Category />
+        </MidCategory>
+      </View>
+      <Grid>
+        <EachButton>
+          <Container
+            bgColor={white}
+            onPress={() => navigation.navigate("A_tab")}
+          >
+            <Adenine />
+          </Container>
+        </EachButton>
+        <EachButton>
+          <Container
+            bgColor={green}
+            onPress={() => navigation.navigate("T_tab")}
+          >
+            <Thymine />
+          </Container>
+        </EachButton>
+      </Grid>
+      <Grid>
+        <EachButton>
+          <Container
+            bgColor={green}
+            onPress={() => navigation.navigate("G_tab")}
+          >
+            <Guanine />
+          </Container>
+        </EachButton>
+        <EachButton>
+          <Container
+            bgColor={white}
+            onPress={() => navigation.navigate("C_tab")}
+          >
+            <Cytosine />
+          </Container>
+        </EachButton>
+      </Grid>
+      <Bottom></Bottom>
+    </>
+  );
+};
