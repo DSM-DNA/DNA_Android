2b24cb18743e5f7101a6e627bc1e18ad1a68e4fc
Ko-GyeongTae <kokt0203@naver.com>
2021-02-19 22:02:53 +0900

    Edit: delete req

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -1,25 +1,30 @@
+import AsyncStorage from "@react-native-community/async-storage";
+import axios from "axios";
 import React from "react";
+import { Alert } from "react-native";
 import styled from "styled-components";
 
+const baseUri = "http://121.66.14.43:9191";
+
 const Container = styled.View`
-    flex: 1;
-    width: 87.2%;
-    height: 80px;
-    margin-top: 2%;
-    margin-bottom: 2%;
-    background-color: white;
-    elevation: 6;
+  flex: 1;
+  width: 87.2%;
+  height: 80px;
+  margin-top: 2%;
+  margin-bottom: 2%;
+  background-color: white;
+  elevation: 6;
 `;
 
 const Header = styled.View`
-    width: 100%;
-    height: 30%;
+  width: 100%;
+  height: 30%;
 `;
 
 const Footer = styled.View`
-    width: 96%;
-    height: 70%;
-    padding-left: 3%;
+  width: 96%;
+  height: 70%;
+  padding-left: 3%;
 `;
 
 const Delete = styled.TouchableOpacity`
@@ -34,38 +39,73 @@ const Delete = styled.TouchableOpacity`
 `;
 
 const WriterBox = styled.View`
-    height: 20px;
+  height: 20px;
 `;
 
 const Writer = styled.Text`
-    color: #204051;
+  color: #204051;
 `;
 
 const Del_Text = styled.Text`
-    color: white;
+  color: white;
 `;
 
 const Content = styled.Text`
-    color: black;
+  color: black;
 `;
 
-
 export default (props) => {
-    return (
-        <Container>
-            <Header>
-                <WriterBox style={{top: 5, left: 6}}>
-                    <Writer style={{fontSize: 15}}>null</Writer>
-                </WriterBox>
-                <Delete style={{top: 3, right: 7}}>
-                    {props.isMine === true ? <Del_Text>삭제하기</Del_Text> : null}
-                </Delete>
-            </Header>
-            <Footer>
-                <Content style={{fontSize: 12}}>
-                    {props.content}
-                </Content>
-            </Footer>
-        </Container>
+  const GetToken = async () => {
+    const token = await AsyncStorage.getItem("jwt");
+    return token;
+  };
+
+  const Del_comment = async () => {
+    const token = await GetToken();
+    const req_token = "Bearer " + token;
+
+    const config = {
+      headers: { Authorization: req_token },
+    };
+
+    await axios
+      .delete(`${baseUri}/comment/${props.commentId}`, config)
+      .catch(function (error) {
+        console.log(error);
+        Alert.alert("게시물을 삭제할 수 없습니다.");
+      });
+  };
+
+  const confirmAlert = () => {
+    Alert.alert(
+      "삭제하시겠습니까?",
+      "",
+      [
+        {
+          text: "No",
+          onPress: () => null,
+        },
+        {
+          text: "Yes",
+          onPress: () => Del_comment(),
+        },
+      ],
+      { cancelable: false }
     );
+  };
+  return (
+    <Container>
+      <Header>
+        <WriterBox style={{ top: 5, left: 6 }}>
+          <Writer style={{ fontSize: 15 }}>null</Writer>
+        </WriterBox>
+        <Delete style={{ top: 3, right: 7 }} onPress={() => confirmAlert()}>
+          {props.isMine === true ? <Del_Text>삭제하기</Del_Text> : null}
+        </Delete>
+      </Header>
+      <Footer>
+        <Content style={{ fontSize: 12 }}>{props.content}</Content>
+      </Footer>
+    </Container>
+  );
 };
