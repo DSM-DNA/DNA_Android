92d8703cdc30243cdb65652cb255e0e665731c59
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 18:48:46 +0900

    <ADD> load gothic fonts

--- assets/Header/CHeader.js
+++ assets/Header/CHeader.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { SvgXml } from "react-native-svg";
+import * as Font from 'expo-font';
 
 const xml = `
   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="129" viewBox="0 0 500 129">
@@ -31,4 +34,25 @@ const xml = `
   </svg>
 `;
 
-export default () => <SvgXml xml={xml} width="100%" height="100%" />;
+export default () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  const loadFonts = async() => {
+    await Font.loadAsync({
+      // Load a font `NanumGothic-Regular` from a static resource
+      NanumGothic: require('../fonts/NanumGothic-Regular.ttf'),
+    });
+    setFontsLoaded(true);
+  }
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if(fontsLoaded){
+    return (
+      <SvgXml xml={xml} width="100%" height="100%" />
+    );
+  } else {
+    return null;
+  }
+};
